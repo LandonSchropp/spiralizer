369550bc68d55a69f23b9ff4b3f74ed0fc8f3bf9
Landon Schropp <schroppl@gmail.com>
2020-06-04 23:44:11 -0700

    Build out downgraded and upgraded cards

--- source/cards.js
+++ source/cards.js
@@ -1,7 +1,52 @@
+import _ from "lodash";
 import scrapeIt from "scrape-it";
 
 const BASE_URL = "https://slay-the-spire.fandom.com/wiki";
 const IRONCLAD_CARDS_URL = `${ BASE_URL }/Ironclad_Cards`;
+
+const PLURAL_REGEX = /(\w+)\((s)\)/g;
+const WORDS_REGEX = /(\w+)\(([^)]+)\)/g;
+const VALUE_REGEX = /(\d*)\((\d+)\)/g;
+const WHITESPACE_REGEX = /\s+/mg;
+
+function downgrade(string) {
+  return string
+    .replace(PLURAL_REGEX, "$1")
+    .replace(VALUE_REGEX, "$1")
+    .replace(WORDS_REGEX, "$1")
+    .replace(WHITESPACE_REGEX, " ");
+}
+
+function upgrade(string) {
+  return string
+    .replace(PLURAL_REGEX, "$1$2")
+    .replace(VALUE_REGEX, "$2")
+    .replace(WORDS_REGEX, "$2")
+    .replace(WHITESPACE_REGEX, " ");
+}
+
+function transformCard({ name, rarity, type, cost, description }) {
+
+  let common = {
+    rarity,
+    type
+  };
+
+  return [
+    {
+      name,
+      description: downgrade(description),
+      ...common,
+      cost: downgrade(cost)
+    },
+    {
+      name: `${ name }+`,
+      description: upgrade(description),
+      ...common,
+      cost: upgrade(cost)
+    }
+  ];
+}
 
 export async function fetchCards() {
 
@@ -10,11 +55,6 @@ export async function fetchCards() {
       listItem: ".article-table tr",
       data: {
         name: "td:nth-of-type(1)",
-        imageURL: {
-          selector: "td:nth-of-type(2) a",
-          attr: "href",
-          convert: url => url.replace(/\/revision.*$/, "")
-        },
         rarity: "td:nth-of-type(3)",
         type: "td:nth-of-type(4)",
         cost: "td:nth-of-type(5)",
@@ -23,5 +63,5 @@ export async function fetchCards() {
     }
   });
 
-  return response.data.cards.slice(1);
+  return _.flatten(response.data.cards.slice(1).map(transformCard));
 }
--- source/csv.js
+++ source/csv.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 import { parse } from "json2csv";
 
 /**
@@ -6,5 +8,22 @@ import { parse } from "json2csv";
  * @return The object as a CSV string.
  */
 export function toCSV(data) {
-  return parse(data, { header: false });
+  return parse(data, { header: true });
+}
+
+/**
+ * Determines if the value is blank.
+ * @value The value to check.
+ * @return True if the value is blank and false otherwise.
+ */
+export function isBlank(value) {
+  return _.isNil(value) || /^\s*$/.test(value);
+}
+
+export function link(title, url) {
+  if (isBlank(title) || isBlank(url)) {
+    return null;
+  }
+
+  return `=HYPERLINK("${ url }", "${ title.replace(/"/g, '""') }")`;
 }
